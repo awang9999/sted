fea4e2e211a2bdc5571f10f5984d4bfb6efa0957
Alexander Wang <alexander.wang2001@gmail.com>
2026-05-19 17:06:44 -0700

    refactored editor and terminal to use crossterm commands

--- src/editor.rs
+++ src/editor.rs
@@ -1,5 +1,12 @@
-use crate::terminal::Terminal;
-use crossterm::event::{Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers, read};
+use crate::terminal::{Terminal, TerminalCoordinate};
+use crossterm::{
+    ExecutableCommand,
+    event::{
+        Event::{self, Key},
+        KeyCode::Char,
+        KeyEvent, KeyModifiers, read,
+    },
+};
 
 pub struct Editor {
     should_quit: bool,
@@ -41,21 +48,25 @@ impl Editor {
         }
     }
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        Terminal::hide_cursor()?;
         if self.should_quit {
             Terminal::clear_screen()?;
             print!("Goodbye.\r\n");
         } else {
+            Terminal::move_cursor_to(TerminalCoordinate { x: 0, y: 0 })?;
             Self::draw_rows()?;
-            Terminal::move_cursor_to(0, 0)?;
+            Terminal::show_cursor()?;
+            Terminal::execute()?;
         }
         Ok(())
     }
     fn draw_rows() -> Result<(), std::io::Error> {
-        let height = Terminal::size()?.1;
+        let height = Terminal::size()?.height;
         for current_row in 0..height {
-            print!("~");
+            Terminal::clear_row(current_row)?;
+            Terminal::print("~")?;
             if current_row + 1 < height {
-                print!("\r\n");
+                Terminal::print("\r\n")?;
             }
         }
         Ok(())
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,8 +1,21 @@
-use crossterm::cursor::MoveTo;
-use crossterm::execute;
+use crossterm::cursor::{Hide, MoveTo, MoveToRow, Show};
+use crossterm::queue;
+use crossterm::style::Print;
 use crossterm::terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode, size};
-use std::io::stdout;
+use std::io::{Write, stdout};
 
+#[derive(Copy, Clone)]
+pub struct TerminalSize {
+    pub height: u16,
+    pub width: u16,
+}
+
+pub struct TerminalCoordinate {
+    pub x: u16,
+    pub y: u16,
+}
+
+#[derive(Copy, Clone)]
 pub struct Terminal {}
 
 impl Terminal {
@@ -13,18 +26,45 @@ impl Terminal {
     pub fn initialize() -> Result<(), std::io::Error> {
         enable_raw_mode()?;
         Self::clear_screen()?;
-        Self::move_cursor_to(0, 0)?;
+        Self::move_cursor_to(TerminalCoordinate { x: 0, y: 0 })?;
+        Self::execute()?;
         Ok(())
     }
     pub fn clear_screen() -> Result<(), std::io::Error> {
-        execute!(stdout(), Clear(ClearType::All))?;
+        queue!(stdout(), Clear(ClearType::All))?;
         Ok(())
     }
-    pub fn move_cursor_to(x: u16, y: u16) -> Result<(), std::io::Error> {
-        execute!(stdout(), MoveTo(x, y))?;
+    pub fn move_cursor_to(c: TerminalCoordinate) -> Result<(), std::io::Error> {
+        queue!(stdout(), MoveTo(c.x, c.y))?;
         Ok(())
     }
-    pub fn size() -> Result<(u16, u16), std::io::Error> {
-        size()
+    pub fn size() -> Result<TerminalSize, std::io::Error> {
+        let (w, h) = size()?;
+        Ok(TerminalSize {
+            width: w,
+            height: h,
+        })
+    }
+    pub fn hide_cursor() -> Result<(), std::io::Error> {
+        queue!(stdout(), Hide)?;
+        Ok(())
+    }
+    pub fn show_cursor() -> Result<(), std::io::Error> {
+        queue!(stdout(), Show)?;
+        Ok(())
+    }
+    pub fn clear_row(row: u16) -> Result<(), std::io::Error> {
+        queue!(stdout(), MoveToRow(row), Clear(ClearType::CurrentLine))?;
+        Ok(())
+    }
+
+    pub fn print(string: &str) -> Result<(), std::io::Error> {
+        queue!(stdout(), Print(string))?;
+        Ok(())
+    }
+
+    pub fn execute() -> Result<(), std::io::Error> {
+        stdout().flush()?;
+        Ok(())
     }
 }
